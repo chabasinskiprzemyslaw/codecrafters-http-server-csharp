89f27c72ef945ef5dde758bc542b3347519469f6
root <root@DESKTOP-JS1CHDV>
2024-05-05 22:25:02 +0200

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -7,7 +7,33 @@ server.Start();
 
 Socket clientSocket = server.AcceptSocket(); // wait for client
 
-string response = "HTTP/1.1 200 OK\r\n\r\n";
+byte[] buffer = new byte[1024];
+int bufferLength = clientSocket.Receive(buffer);
+string request = Encoding.UTF8.GetString(buffer, 0, bufferLength);
+string[] splitRequest = request.Split("\n");
+
+//GET / HTTP/1.1
+//Host: localhost:4221
+//User-Agent: curl/8.4.0
+//Accept: */*
+
+//GET / HTTP/1.1
+string startLine = splitRequest[0];
+
+string[] splitStartLine = startLine.Split(" ");
+
+string requestPath = splitStartLine[1];
+
+string response;
+if (requestPath != "/")
+{
+    response = "HTTP/1.1 404 Not Found\r\n\r\n";
+}
+else
+{
+    response = "HTTP/1.1 200 OK\r\n\r\n";
+}
+
 byte[] responseBytes = Encoding.UTF8.GetBytes(response);
 clientSocket.Send(responseBytes);
 
